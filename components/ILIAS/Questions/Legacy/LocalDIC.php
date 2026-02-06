ea176d1b6afce368e69a8de2203619f9f824eac0
Stephan Kergomard <webmaster@kergomard.ch>
2026-02-06 16:31:09 +0100

    Questions: Introduce Persistence Factory

--- components/ILIAS/Questions/Legacy/LocalDIC.php
+++ components/ILIAS/Questions/Legacy/LocalDIC.php
@@ -25,6 +25,7 @@ use ILIAS\Questions\Persistence\Repository as QuestionsRepository;
 use ILIAS\Questions\AnswerFormTypes\Cloze;
 use ILIAS\Questions\Persistence\TableNameSpaceCore;
 use ILIAS\Questions\AnswerForm\Factory as AnswerFormFactory;
+use ILIAS\Questions\Persistence\Factory as PersistenceFactory;
 use ILIAS\Questions\Presentation\Views\Edit;
 use ILIAS\Questions\Presentation\Layout\Factory as LayoutFactory;
 use ILIAS\Questions\Units\Repository as UnitsRepository;
@@ -32,7 +33,6 @@ use ILIAS\Questions\UserSettings\CreateMode;
 use ILIAS\Data\Factory as DataFactory;
 use ILIAS\Data\UUID\Factory as UuidFactory;
 use ILIAS\DI\Container as ILIASContainer;
-use ILIAS\User\Settings\Settings as UserSettings;
 use Mustache\Engine as MustacheEngine;
 use Pimple\Container as PimpleContainer;
 
@@ -61,7 +61,6 @@ class LocalDIC extends PimpleContainer
             $DIC['lng'],
             $DIC['ilDB']
         );
-
         $dic[ConfigurationRepository::class] = static fn($c): ConfigurationRepository
             => new ConfigurationRepository(
                 $DIC['ilSetting'],
@@ -81,7 +80,8 @@ class LocalDIC extends PimpleContainer
             new QuestionsRepository(
                 $DIC['ilDB'],
                 $DIC['refinery'],
-                new UuidFactory(),
+                $c[UuidFactory::class],
+                new PersistenceFactory(),
                 $c[AnswerFormFactory::class]
             );
         $dic[LayoutFactory::class] = static fn($c): LayoutFactory =>
